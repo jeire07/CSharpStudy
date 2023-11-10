547191370ae1dbe547ee9bc162e47733accb40d3
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-11-10 21:11:54 +0900

    TicTacToe&Main modify

--- Game01TicTacToe.cs
+++ Game01TicTacToe.cs
@@ -1,116 +1,93 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace CSharpStudy
 {
     public class Game01TicTacToe
     {
-        static string[] Mark = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-        static int[,] Owner = new int[9, 3];
-        static int count = 1;
-        static int player = 0;
-        static int userNum = 0;
-        static int whoWin = 0;
+        private string[] _mark = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private int _count = 1;
+        private int _player = 0;
+        private int _userNum = 0;
+        private int _whoWin = 0;
 
-        static void InitOwner()
+        public void PlayTicTacToe()
         {
-            for (int i = 0; i < 9; i++)
+            while(true)
             {
-                for (int j = 1; j > -2; j--)
-                {
-                    Owner[i, 0] = (i % 3) - 1;
-                    Owner[i, 1] = (-i / 3) + 1;
-                    Owner[i, 2] = 0;
-                }
-            }
-        }
+                Clear();
+                WriteLine();
+                WriteLine(" Two players compete to make line");
+                WriteLine(" If a player gets three in a row, that player wins.");
+                
+                DisplayBoard();
 
-        static void WriteOwner()
-        {
-            Console.Write("{");
-            for (int i = 0; i < 9; i++)
-            {
-                Console.Write("{");
-                for (int j = 0; j < 3; j++)
+                while (_whoWin == 0)
                 {
-                    if (j == 2)
-                    {
-                        Console.Write(Owner[i, j]);
-                    }
-                    else
-                    {
-                        Console.Write(Owner[i, j] + ", ");
-                    }
+                    _player = ((_count % 2) == 1) ? 1 : 2;
+
+                    Write(" player{0} pick : ", _player);
+                    _userNum = GetInput(1, 9, _player);
+
+                    Clear();
+                    DisplayBoard();
+
+                    _whoWin = CheckWin(_player);
                 }
-                if (i == 8)
+
+                if ((_whoWin == 1) || (_whoWin == 2))
                 {
-                    Console.Write("}");
+                    WriteLine("player{0} wins!", _whoWin);
                 }
                 else
                 {
-                    Console.Write("}, ");
+                    WriteLine("tie!");
+                }
+
+                Write(" Play one more time? (y/n): ");
+                string userInput = ReadLine();
+
+                if (userInput.ToLower() == "y")
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        _mark[i] = i.ToString();
+                        _count = 1;
+                        _whoWin = 0;
+                        _player = 0;
+                        _userNum = 0;
+                    }
+                }
+                else
+                {
+                    break;
                 }
             }
-            Console.WriteLine("}");
-            Console.WriteLine();
         }
 
-        static void PlayTicTacToe()
+        private void DisplayBoard()
         {
-            Console.Clear();
-            InitOwner();
-            WriteOwner();
-
-            Console.WriteLine(" Two players compete to make line");
-            Console.WriteLine(" If a player gets three in a row, that player wins.");
-            Console.WriteLine();
-            DisplayBoard();
-
-
-            while (whoWin == 0)
-            {
-                player = ((count % 2) == 1) ? 1 : 2;
-
-                Console.Write(" player{0} pick : ", player);
-                userNum = GetInput(1, 9, player);
-
-                Console.Clear();
-                WriteOwner();
-                DisplayBoard();
-
-                whoWin = CheckWin(player);
-            }
-
-            if ((whoWin == 1) || (whoWin == 2))
-            {
-                Console.WriteLine("player{0} wins!", whoWin);
-            }
-            else
-            {
-                Console.WriteLine("tie!");
-            }
-        }
-
-        static void DisplayBoard()
-        {
+            WriteLine();
             for (int i = 0; i < 9; i++)
             {
                 if (((i % 3) == 0) || (i == 8))
                 {
-                    Console.WriteLine("     |     |     ");
+                    WriteLine("      |     |     ");
                 }
                 else if ((i % 3) == 1)
                 {
-                    Console.WriteLine("  {0}  |  {1}  |  {2}  ", Mark[i], Mark[i+1], Mark[i+2]);
+                    WriteLine("   {0}  |  {1}  |  {2}  "
+                        , _mark[i], _mark[i+1], _mark[i+2]);
                 }
                 else
                 {
-                    Console.WriteLine("_____|_____|_____");
+                    WriteLine(" _____|_____|_____");
                 }
             }
-            Console.WriteLine();
+            WriteLine();
         }
 
-        static int GetInput(int minimum, int maximum, int player)
+        private int GetInput(int minimum, int maximum, int player)
         {
             string userInput;
             int validInput = 0;
@@ -118,10 +95,10 @@ namespace CSharpStudy
             while (validInput != 1)
             {
                 //get user's input
-                userInput = Console.ReadLine();
+                userInput = ReadLine();
 
                 //exception by using tryParse
-                validInput = int.TryParse(userInput, out userNum) ? 1 : 0;
+                validInput = int.TryParse(userInput, out _userNum) ? 1 : 0;
 
                 /*
                 //check user's input is integer
@@ -132,73 +109,102 @@ namespace CSharpStudy
                 }
                 catch
                 {
-                    Console.WriteLine(" you can input integer only");
+                    WriteLine(" you can input integer only");
                 }
                 */
 
                 //check user's input is between 1 and 9
                 if (validInput == 0)
                 {
-                    Console.WriteLine(" you can input integer only");
-                    Console.WriteLine();
-                    Console.Write(" player{0} pick : ", player);
+                    WriteLine(" you can input integer only");
+                    WriteLine();
+                    Write(" player{0} pick : ", player);
                 }
-                else if ((userNum > maximum) || (userNum < minimum))
+                else if ((_userNum > maximum) || (_userNum < minimum))
                 {
-                    Console.WriteLine(" that number is out of range");
-                    Console.Write(" choose another number ");
-                    Console.WriteLine("between 1 to 9");
-                    Console.WriteLine();
-                    Console.Write(" player{0} pick : ", player);
+                    WriteLine(" that number is out of range");
+                    Write(" choose another number ");
+                    WriteLine("between 1 to 9");
+                    WriteLine();
+                    Write(" player{0} pick : ", player);
                     validInput = 0;
                 }
                 else
                 {
-                    if ((Mark[userNum] == "X") || (Mark[userNum] == "O"))
+                    if ((_mark[_userNum] == "X") || (_mark[_userNum] == "O"))
                     {
-                        Console.WriteLine("you cannot select that point, select another point");
-                        Console.WriteLine();
-                        Console.Write(" player{0} pick : ", player);
+                        WriteLine("you cannot select that point, select another point");
+                        WriteLine();
+                        Write(" player{0} pick : ", player);
                     }
                     else
                     {
                         if (player % 2 == 0)
                         {
-                            Mark[userNum] = "O";
-                            Owner[userNum - 1, 2] = -1;
+                            _mark[_userNum] = "O";
                         }
                         else
                         {
-                            Mark[userNum] = "X";
-                            Owner[userNum - 1, 2] = 1;
+                            _mark[_userNum] = "X";
                         }
-                        count++;
+                        _count++;
                         break;
                     }
-                    Console.WriteLine();
+                    WriteLine();
                 }
             }
-            return userNum;
+            return _userNum;
         }
 
-        static int CheckWin(int player)
+        private int CheckWin(int player)
         {
+            int[] owner = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             int[] line = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-            for (int i = 0; i < 3; i++)
+            //Verify ownership for each cell
+            for (int i = 1; i < 10; i++)
             {
-                for (int j = 0; j < 3; j++)
+                if (_mark[i] == "X")
                 {
-                    line[i] += Owner[3 * i + j, 2];
-                    line[3+j] += Owner[3 * i + j, 2];
+                    owner[i-1] = 1;
+                }
+                else if (_mark[i] == "O")
+                {
+                    owner[i-1] = -1;
+                }
+                else
+                {
+                    owner[i-1] = 0;
                 }
             }
-            line[6] = Owner[0, 2] + Owner[4, 2] + Owner[8, 2];
-            line[7] = Owner[2, 2] + Owner[4, 2] + Owner[6, 2];
+
+            //calculate ownership for each line
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    line[j] += owner[3 * j + k];
+                    line[3+k] += owner[3 * j + k];
+                }
+            }
+            line[6] = owner[0] + owner[4] + owner[8];
+            line[7] = owner[2] + owner[4] + owner[6];
+
+            //if you want to see Console Write for CheckWin, uncomment below codes.
+            //for (int i = 0; i < owner.Length; i++)
+            //{
+            //    Console.Write(owner[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //for (int i = 0; i < line.Length; i++)
+            //{
+            //    Console.Write(line[i] + " ");
+            //}
+            //Console.WriteLine();
 
             for (int i = 0; i < 8; i++)
             {
-
                 if (line[i] == 3)
                 {
                     return 1;
@@ -209,7 +215,7 @@ namespace CSharpStudy
                 }
                 else
                 {
-                    if (count == 9)
+                    if (_count == 10)
                     {
                         return 3;
                     }
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace CSharpStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // If you try Tic-Tac-Toe code, uncomment below two lines
+            Game01TicTacToe tictactoe = new Game01TicTacToe();
+            tictactoe.PlayTicTacToe();
+
+            // If you try Blackjack code, uncomment below two lines
+            //Game03Blackjack game = new Game03Blackjack();
+            //game.PlayGame();
+
+            // If you try Snake code, uncomment below two lines
+            //Game02Snake game = new Game02Snake();
+            //game.
+        }
+    }
+}
